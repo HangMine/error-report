ffac552debedb1db727ac75b5fdd770a5a497ec0
HangMine <95629358@qq.com>
2020-10-17 12:19:46 +0800

    修复stacks信息里面没有文件信息的情况

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const { spawnSync } = require('child_process');
 const params = [{ "versionHash": "cfebe8c9be38ad07a7638b0578545f55c3100c1e", "env": "development", "userAgent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36", "componentTree": "? > Error-test", "file": "", "message": "a is not defined", "stack": "ReferenceError: a is not defined\n    at http://dev.4dshoetech.local/js/chunk-577474a8.0a9d5ce5.js:1:639\n    at u (http://dev.4dshoetech.local/js/chunk-vendors.35e84e3b.js:54:4662)\n    at Generator._invoke (http://dev.4dshoetech.local/js/chunk-vendors.35e84e3b.js:54:5988)\n    at Generator.next (http://dev.4dshoetech.local/js/chunk-vendors.35e84e3b.js:54:5087)\n    at i (http://dev.4dshoetech.local/js/chunk-vendors.35e84e3b.js:11:15186)\n    at s (http://dev.4dshoetech.local/js/chunk-vendors.35e84e3b.js:11:15389)\n    at http://dev.4dshoetech.local/js/chunk-vendors.35e84e3b.js:11:15448\n    at new Promise (<anonymous>)\n    at http://dev.4dshoetech.local/js/chunk-vendors.35e84e3b.js:11:15329\n    at a.mounted (http://dev.4dshoetech.local/js/chunk-577474a8.0a9d5ce5.js:1:684)", "position": "vue: mounted hook (Promise/async)", "name": "ReferenceError", "level": 0, "project": "art", "ref": "hang/1.19.0" }]
 
 
-const { stdout, stderr } = spawnSync('node', ['getStackSource', JSON.stringify(params), '/'], { encoding: 'utf-8' })
+const { stdout, stderr } = spawnSync('node', ['getStackSource', JSON.stringify(params), '/Users/Administrator/Desktop/error-report'], { encoding: 'utf-8' })
 
 console.log('---------stdout---------');
 console.log(stdout)
--- utils/source.js
+++ utils/source.js
@@ -29,30 +29,26 @@ const getMapPath = ({ fileName, basePath, project, versionHash }) => {
   const mapFileName = `${fileName}.map`;
   let mapPath = basePath ? join(basePath, project, versionHash, 'js', mapFileName) : `${fileName}.map`;
   if (isHttpBasePath) mapPath = path2HttpPath(mapPath);
-  console.log('fileName文件名字：\n', fileName);
-  console.log('mapPath地址：\n', mapPath);
+  // console.log('fileName文件名字：\n', fileName);
+  // console.log('mapPath地址：\n', mapPath);
   return mapPath;
 }
 
 const getSourceInfos = async ({ stack, project, basePath, versionHash }) => {
   let sourceInfos = []
   const stacks = new Stacktracey(stack).items; // 解析错误信息
-
   for (const item of stacks) {
     const { file, line, column, fileName } = item;
     // 排除node_modules的堆栈：chunk-vendors可能包含非node_modules的公共模块,需检查vue-cli3的webpack配置
-    if (file.includes('chunk-vendors')) continue;
-
-    console.log("item:\n", item)
+    // 解析出来的item可能出现没有fileName的情况：比如 at new Promise (<anonymous>) ，通过fileName忽略这种情况
+    if (file.includes('chunk-vendors') || !fileName) continue;
 
     const sourceMap = await getSourceMap({ path: getMapPath({ fileName, basePath, project, versionHash }), project });
-
     const sourceInfo = await SourceMapConsumer.with(sourceMap, null, consumer => {
       const _sourceInfo = consumer.originalPositionFor({ line, column });
       _sourceInfo.source = _sourceInfo.source.replace('webpack:///', '').replace(/\?.*$/, '');
       return _sourceInfo;
     });
-
 
     // 排除node_modules的堆栈：需要先请求每一个map文件拿到源文件，比较慢
     // if (sourceInfo.source.includes('node_modules')) continue;
